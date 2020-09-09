b3d17e20c90c9684905b169d81bc34c955ebbe86
Bornstein <sara.bornstein@alaska.gov>
2020-09-09 09:42:25 -0800

    second commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random (movies controller with Action called Random)
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel); //pass the movie object here
+        }
+
+        //attribute route
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //This action will be called when we navigate to movies.
+        //It will return a list of movies in the database.
+        //To make parameters optional, make it nullable:
+        //add ? after int to make it nullable.
+        //string is a reference type and nullable in C#
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            //check to see if parameters have a value
+            if (!pageIndex.HasValue)
+                pageIndex = 1; //if no value, initialize with 1
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            //no parameters specified: pageIndex=1&sortBy=Name
+            //specify parameters in web browser: localhost/movies?pageIndex=2&sortBy=ReleaseDate --> pageIndex=2&sortBy=ReleaseDate
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Vidly
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+           
+
+            /*
+             app.UseEndpoints(endpoints =>
+             {
+                 //define a custom route: this is not the best way. 
+                 endpoints.MapControllerRoute(
+                     //routes to ByReleaseDate controller which returns year and month
+                     "MoviesByReleaseDate",
+                     "movies/released/{year}/{month}",
+                     new { controller = "Movies", action = "ByReleaseDate" },
+                     //constraints: year must have 4 digits, month must have 2 digits
+                     //use regular expression (regex)
+                     new { year = @"\d{4}", month = @"\d{2}" }); // \d = digit and 4 = number of repetitions
+             });
+             */
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
